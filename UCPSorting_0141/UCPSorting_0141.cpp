8ecb55b2f439b84ffb2bc15a62c8b6f6cdaa299f
HaritsTama <nayotama1818@gmail.com>
2024-03-22 10:00:51 +0700

    Menambahkan variable global "temp" dan membuat prosedur input

--- UCPSorting_0141/UCPSorting_0141.cpp
+++ UCPSorting_0141/UCPSorting_0141.cpp
@@ -11,4 +11,25 @@ using namespace std;
 // 3. Dengan menggunakan pass, pass bisa dibilang mirip dengan putaran yang dilakukan. Kita bisa meletakan pass ini di akhir kode sortir kita dengan menaikan nilai nya + 1 setiap putaran sortir
 // pass bisa dihitung dengan n - 1
 
-int j, n, i, min_index, Nayo[41];
+int j, n, i, min_index, Nayo[41], temp;
+
+void input() {
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array = ";
+		cin >> n;
+		if (n <= 41)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 41 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << " : ";
+		cin >> Nayo[i];
+	}
+}
